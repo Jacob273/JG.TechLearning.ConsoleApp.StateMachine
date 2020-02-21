99e1803a22503e723d5ec8de2fa78af65195b6e9
Jakub Gmur <jakubgmur@Op.pl>
2020-02-21 01:08:19 +0100

    Initial checkin + interfaces

---
+++ JG.TechLearning.ConsoleApp.StateMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JG.TechLearning.ConsoleApp.StateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ JG.TechLearning.ConsoleApp.StateMachine/VirtualState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JG.TechLearning.ConsoleApp.StateMachine
+{
+    public abstract class VirtualState : IState
+    {
+
+    }
+}
